968960b8000eff9bed984afeae9515a36bd01f41
MOBOLA MORONTO <t2wycemo@gmail.com>
2020-10-01 10:33:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 function iterativeLog(array) {
-  array.forEach((array[i], i) => {
-  console.log(`${index}: ${element}`);
-  })
-}
-
-
-function callback(iterativeLog, index, array) {
-  console.log(`${index}: ${element}`);
+  array.forEach((element, index) => {
+  console.log(`${index}: ${element}`)
+  });
 }
 
 function iterate(callback) {
-  var candy = ['twix', 'm&m\'s', 'butterfingers']
-  candy.forEach(callback)
+  var candy = ['twix', 'm&m\'s', 'butterfingers'];
+  candy.forEach(callback);
   return candy;
 }
 
